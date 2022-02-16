7af1e54a6907ed6a58d7a813c62578a79b0a4230
Sherislam <sherislam02@yandex.ru>
2022-02-16 15:35:54 +0600

    Initial Commit

---
+++ test12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test12
+//
+//  Created by sher on 16/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
