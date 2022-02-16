1f088422b4d91a01c69fc04c0254aab987c33db9
Sherislam <sherislam02@yandex.ru>
2022-02-16 18:46:40 +0600

    first app

--- test12/ViewController.swift
+++ test12/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var ShowButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelText.isHidden = true
+        ShowButton.layer.cornerRadius = 8
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clicker() {
+        if labelText.isHidden {
+            labelText.isHidden = false
+            ShowButton.setTitle("Hidden text", for: .normal)
+        }   else {
+            labelText.isHidden = true
+            ShowButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 }
 
