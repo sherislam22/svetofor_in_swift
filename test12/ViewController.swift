662037bfadfd5f7976605df48d35c184b69feb54
Sherislam <sherislam02@yandex.ru>
2022-02-20 17:50:46 +0600

    светофор

--- test12/ViewController.swift
+++ test12/ViewController.swift
@@ -8,24 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var labelText: UILabel!
-    @IBOutlet weak var ShowButton: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        labelText.isHidden = true
-        ShowButton.layer.cornerRadius = 8
-        // Do any additional setup after loading the view.
+    enum CurrentView {
+        case red, yellow, green
     }
 
-    @IBAction func clicker() {
-        if labelText.isHidden {
-            labelText.isHidden = false
-            ShowButton.setTitle("Hidden text", for: .normal)
-        }   else {
-            labelText.isHidden = true
-            ShowButton.setTitle("Show text", for: .normal)
+    @IBOutlet weak var RedviewLabel: UIView!
+    
+    @IBOutlet weak var GreenviewLabel: UIView!
+    @IBOutlet weak var YellowviewLabel: UIView!
+    
+    @IBOutlet weak var StartButton: UIButton!
+    
+    private var currentLight = CurrentView.red
+    private let LightOn:CGFloat = 1
+    private let LightOff: CGFloat = 0.3
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        RedviewLabel.alpha = LightOff
+        YellowviewLabel.alpha = LightOff
+        GreenviewLabel.alpha = LightOff
+        
+        
+    }
+    override func viewDidLayoutSubviews() {
+        RedviewLabel.layer.cornerRadius = RedviewLabel.frame.height / 2
+        YellowviewLabel.layer.cornerRadius = RedviewLabel.frame.height / 2
+        GreenviewLabel.layer.cornerRadius = RedviewLabel.frame.height / 2
+    }
+    @IBAction func NextButton() {
+        StartButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            GreenviewLabel.alpha = LightOff
+            currentLight = .yellow
+            RedviewLabel.alpha = LightOn
+        case .yellow:
+            
+            YellowviewLabel.alpha = LightOn
+            currentLight = .green
+            RedviewLabel.alpha = LightOff
+        case .green:
+            GreenviewLabel.alpha = LightOn
+            YellowviewLabel.alpha = LightOff
+            currentLight = .red
         }
+        
     }
     
 }
